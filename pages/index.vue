5b85c95b88e3ebde0aec48d27613c1c37a8887e6
jk-20200102 <jk.20200102@gmail.com>
2023-07-20 14:10:13 +0900

    통신, 상태관리 등 샘플코드 추가

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,23 @@
   <div>
     <!-- font-weight: bold; font-size: 1.875rem; -->
     <h1 class="font-bold text-3xl">Home</h1>
+    <!-- <div v-if="pending">Loading..</div>
+      <template v-else>
+        <div v-if="error">Sorry, error occured.</div>
+        <div v-else>{{ data }}</div>
+      </template>
+
+      <div>{{ userStore.user }}</div>
+
+      <button @click="refresh()">Refresh</button>
+      <button @click="userStore.save({ email: 'peterkimzz69@gmail.com' })">Increment</button> -->
   </div>
 </template>
+
+<script setup lang="ts">
+  // import { useUserStore } from '~~/stores/user'
+
+  // const { data, pending, error, refresh } = await useApi('/todos/1')
+
+  // const userStore = useUserStore()
+</script>
