dc3585e4262fd1e33386d19ddb3b91f22bce9f4f
jk-20200102 <jk.20200102@gmail.com>
2023-07-20 13:26:34 +0900

    1st commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <nav>
+      <NuxtLink to="/login">Go to login</NuxtLink>
+    </nav> -->
+    <NavigationBar />
+
+    <div>index</div>
+  </div>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Go to index</NuxtLink>
+    </nav>
+
+    <div>login</div>
+  </div>
+</template>
